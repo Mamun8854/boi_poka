74269e65c5a18939e65457f8fb85b9d812bb5500
Mamun8854 <mamunabdullah8854@gmail.com>
2022-11-27 00:46:59 +0600

    seller route added

--- src/Routes/SellerRoute/SellerRoute.js
+++ src/Routes/SellerRoute/SellerRoute.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../../Context/AuthProvider";
+import UseSeller from "../../Hooks/UseSeller/UseSeller";
+import Loading from "../../Pages/Shared/Loading/Loading";
 
-const SellerRoute = () => {
-  return <div></div>;
+const SellerRoute = ({ children }) => {
+  const { user, loading, logOutUser } = useContext(AuthContext);
+  const [isSeller, isSellerLoading] = UseSeller(user?.email);
+  const location = useLocation();
+
+  if (loading || isSellerLoading) {
+    return <Loading></Loading>;
+  }
+
+  if (!isSeller) {
+    return logOutUser();
+  }
+
+  if (isSeller) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
 
 export default SellerRoute;
