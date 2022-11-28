dde01a93232a7a7f238f9f0b42d102f794483879
Mamun8854 <mamunabdullah8854@gmail.com>
2022-11-28 22:39:05 +0600

    design changed for subscribe section

--- src/Pages/Home/Section3/Section3.js
+++ src/Pages/Home/Section3/Section3.js
@@ -7,9 +7,9 @@ const Section3 = () => {
     <section className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
       <div className="container px-6 py-16 mx-auto">
         <div className="items-center lg:flex">
-          <div className="w-full lg:w-1/2">
+          <div className="w-full lg:w-1/2 bg-gray-100 px-10 py-28 rounded-xl">
             <div className="lg:max-w-lg">
-              <h1 className="text-2xl font-semibold text-gray-800 dark:text-white lg:text-3xl">
+              <h1 className="text-2xl font-semibold text-black  lg:text-3xl">
                 Subscribe For Get Update
               </h1>
 
@@ -17,7 +17,7 @@ const Section3 = () => {
                 <input
                   id="email"
                   type="text"
-                  className="px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+                  className="px-4 py-2 text-gray-400 bg-white border rounded-md dark:bg-white dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
                   placeholder="Email Address"
                 />
 
