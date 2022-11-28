1d1314cd1b9cb5a3e300b29bd7123823875aab55
Mamun8854 <mamunabdullah8854@gmail.com>
2022-11-28 22:35:26 +0600

    design changed for advertise item

--- src/Pages/Dashboard/Seller/AddvertiseItem/AddvertiseItem.js
+++ src/Pages/Dashboard/Seller/AddvertiseItem/AddvertiseItem.js
@@ -17,7 +17,6 @@ const AddvertiseItem = () => {
   // console.log(books);
   return (
     <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 ">
-      <h2>Advertise Items Here</h2>
       <div className="grid grid-cols-3">
         {books?.map((book) => (
           <div className={(book?.advertise && !book?.paid) || "hidden"}>
